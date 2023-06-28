b5407791bdefbf64a2943f6f44dee4a7dde0b2a3
menuHwang <hmin0206@naver.com>
2023-06-28 17:25:50 +0900

    [Feat] 매치 조회 api 연결

--- src/pages/admin/matches/index.tsx
+++ src/pages/admin/matches/index.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NavbarProvider from "../../../components/navbar/NavbarProvider";
 import Sidebar from "../../../components/admin/sidebar";
 import QueryInfo from "./QueryInfo";
 import QueryMatch from "./QueryMatch";
+import {Match} from "../../../types/Match";
+import {fetchGetMatches} from "../../../api/matches";
+import {useSearchParams} from "react-router-dom";
 
-const mockMatches = [{
-    id: "b1e46963-59ec-42f2-87d7-f22248c7f411",
-    createdAt: "2023-06-25 11:13:32"
-}, {
-    id: "d89f8d18-d2f8-4b30-a433-f6033f1a6600",
-    createdAt: "2023-06-25 10:51:47"
-}]
 
 function AdminMatchesPage() {
+    const [searchParams] = useSearchParams();
+    const pageParam: string | null = searchParams.get('page');
+    const page: number = pageParam === null ? 0 : parseInt(pageParam);
+    const sizeParam: string | null = searchParams.get('size');
+    const size: number = sizeParam === null ? 20 : parseInt(sizeParam);
+
+    const [matches, setMatches] = useState<Match[]>([]);
+    const [total, setTotal] = useState<number>(0);
+
+    useEffect(() => {
+        fetchGetMatches(page, size, setMatches, setTotal);
+    }, [page, size])
+
     return (
         <>
             <NavbarProvider isAdmin={true} />
@@ -23,8 +32,8 @@ function AdminMatchesPage() {
                     </aside>
                     <div className="col-auto vstack gap-4">
                         <h1>매치 관리</h1>
-                        <QueryInfo count={0} />
-                        <QueryMatch matches={mockMatches} />
+                        <QueryInfo size={total} />
+                        <QueryMatch matches={matches} offset={page * size} />
                     </div>
                 </div>
             </div>

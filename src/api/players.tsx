41fb81fcd7a2ded9f2dc85776c7209bc3edc3b3b
menuHwang <hmin0206@naver.com>
2023-07-07 15:17:26 +0900

    [Feat] 플레이어 전적 갱신 버튼 핸들러 추가

--- src/api/players.tsx
+++ src/api/players.tsx
@@ -25,3 +25,15 @@ export function fetchSearchPlayer(
         setResult(data.result.matches);
     }).catch(e => console.log(e))
 }
+
+export function fetchUpdateMatchHistory(
+    nickname: string | undefined,
+    callback: () => void
+): Promise<void> {
+    return useAxios.patch(ENDPOINT.PLAYERS + `/${nickname}`)
+        .then(() => {
+            callback();
+    }).catch(() => {
+        alert('전적 갱신에 실패했습니다. 잠시 후 다시 시도해주세요.')
+    })
+}
--- src/pages/player/PlayerTag.tsx
+++ src/pages/player/PlayerTag.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 
 type PlayerTagProps = {
-    nickname: string | undefined
+    nickname: string | undefined,
+    loading: boolean,
+    updateBtnHandler: () => void
 }
 
-function PlayerTag({nickname}: PlayerTagProps) {
+function PlayerTag(props: PlayerTagProps) {
     return (
         <div className="card">
             <div className="card-body">
-                <h1 className="mb-3" id="nickname">{nickname}</h1>
+                <h1 className="mb-3" id="nickname">{props.nickname}</h1>
                 <div className="d-flex">
-                    <button className="pa-renew-btn btn btn-primary me-2">
+                    <button className="pa-renew-btn btn btn-primary me-2" disabled={props.loading} onClick={props.updateBtnHandler}>
                         전적 갱신
-                        <span className="spinner-border spinner-border-sm" role="status" style={{display: "none"}}></span>
+                        <span className="spinner-border spinner-border-sm" role="status"
+                              style={{display: props.loading ? "inline-flex" : "none"}}></span>
                     </button>
-                    <a className="btn btn-warning d-flex align-items-center" role="button">
+                    <button className="btn btn-warning d-flex align-items-center">
                         <i className="fa-solid fa-bookmark"></i>
-                    </a>
+                    </button>
                 </div>
             </div>
         </div>

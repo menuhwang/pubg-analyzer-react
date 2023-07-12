c606f9beb99b9ff6ba3b4790d22e95991a0f228f
menuHwang <hmin0206@naver.com>
2023-07-12 23:00:27 +0900

    [Feat] MatchType 추가

--- src/types/MatchInfo.tsx
+++ src/types/MatchInfo.tsx
@@ -1,8 +1,10 @@
 import {GameMode} from "./GameMode";
 import {MapName} from "./MapName";
+import {MatchType} from "./MatchType";
 
 export type MatchInfo = {
     id: string,
+    matchType: MatchType,
     map: MapName,
     mode: GameMode,
     createdAt: string,

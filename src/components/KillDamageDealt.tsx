5c4a3b9245543a9ba3b01ff3b4eaee5474c28f06
menuHwang <hmin0206@naver.com>
2023-07-12 23:01:13 +0900

    [Fix] KillDamageDealt 테이블 사이즈 및 소수점 자릿수 제한

--- src/components/KillDamageDealt.tsx
+++ src/components/KillDamageDealt.tsx
@@ -9,21 +9,23 @@ type KillDamageDealtProps = {
 function KillDamageDealt(props: KillDamageDealtProps) {
     if (props.member === undefined || props.member.length === 0) return null;
 
-    const th: ReactElement[] = props.member.map(nickname => <th key={nickname} style={{width: "25%"}}>{nickname}</th>);
-    const td: ReactElement[] = props.member.map(nickname => <td key={nickname}>{props.playerDamageDealt[nickname] !== undefined ? props.playerDamageDealt[nickname] : "0.0"}</td>);
+    const colWidth = (100 / (props.member.length + 1)).toFixed(2);
+
+    const th: ReactElement[] = props.member.map(nickname => <th key={nickname} style={{width: `${colWidth}%`}}>{nickname}</th>);
+    const td: ReactElement[] = props.member.map(nickname => <td key={nickname}>{props.playerDamageDealt[nickname] !== undefined ? props.playerDamageDealt[nickname].toFixed(2) : "0.0"}</td>);
 
     return (
         <div className="table-responsive">
             <table className="table align-middle">
                 <thead>
                 <tr>
-                    <th style={{width: "25%"}}>{props.player}</th>
+                    <th style={{width: `${colWidth}%`}}>{props.player}</th>
                     {th}
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
-                    <td>{props.playerDamageDealt[props.player] !== undefined ? props.playerDamageDealt[props.player] : "0.0"}</td>
+                    <td>{props.playerDamageDealt[props.player] !== undefined ? props.playerDamageDealt[props.player].toFixed(2) : "0.0"}</td>
                     {td}
                 </tr>
                 </tbody>

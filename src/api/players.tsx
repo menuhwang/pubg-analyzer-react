ca4f6abd8b40c3b03e83f01dabbf65179f0dbd23
menuHwang <hmin0206@naver.com>
2023-07-27 18:46:36 +0900

    [Feat] 존재하지 않는 플레이어 검색 에러 처리

--- src/api/players.tsx
+++ src/api/players.tsx
@@ -8,7 +8,11 @@ export function fetchUpdateMatchHistory(
     return useAxios.patch(ENDPOINT.PLAYERS + `/${nickname}`)
         .then(() => {
             callback();
-    }).catch(() => {
-        alert('전적 갱신에 실패했습니다. 잠시 후 다시 시도해주세요.')
+    }).catch((error) => {
+        if (error.response.status === 404) {
+            alert('해당 플레이어를 찾을 수 없습니다.');
+            return;
+        }
+        alert('전적 갱신에 실패했습니다. 잠시 후 다시 시도해주세요.');
     })
 }
